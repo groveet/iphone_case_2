80566049b57123f4f2d08579f52e2fcdbe641681
Anton Liforenko <groveet2@gmail.com>
2015-08-27 15:43:57 +0300

    HAML and SCSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,51 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+
+	/***************** Waypoints ******************/
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInRight');
+	}, {
+		offset: '75%'
+	});
+	$('.wp10').waypoint(function() {
+		$('.wp10').addClass('animated flipInX');
+	}, {
+		offset: '75%'
+	});
+	$('.wp11').waypoint(function() {
+		$('.wp11').addClass('animated flipInX');
+	}, {
+		offset: '75%'
+	});
+	$('.wp12').waypoint(function() {
+		$('.wp12').addClass('animated flipInY');
+	}, {
+		offset: '75%'
+	});
+	$('.wp13').waypoint(function() {
+		$('.wp13').addClass('animated flipInY');
+	}, {
+		offset: '75%'
+	});
+
+
+
+});
