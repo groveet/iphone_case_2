cdd2673b3aae908a351babf63e9ff2d4129c55a3
Anton Liforenko <groveet2@gmail.com>
2015-08-28 16:26:03 +0300

    mzfk

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
 	belongs_to :category
 	has_attached_file :image, styles: { medium: "500x500>", thumb: "200x200>" }
+					  :bucket => 'rails-app-production',
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
