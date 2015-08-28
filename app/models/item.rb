9b929c523dced220c177851bcd06db4c82b2012a
Anton Liforenko <groveet2@gmail.com>
2015-08-28 16:30:45 +0300

    zaeb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
 	belongs_to :category
-	has_attached_file :image, styles: { medium: "500x500>", thumb: "200x200>" }
-					  :bucket => 'rails-app-production',
+	has_attached_file :image, 
+					  :styles => { medium: "500x500>", thumb: "200x200>" },
+					  :bucket => 'rails-app-production'
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
