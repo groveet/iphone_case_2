da2b33c7fb9e107351a65f2b2bc7f03b8d996f6d
Anton Liforenko <groveet2@gmail.com>
2015-09-04 18:10:14 +0300

    mas

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
 	belongs_to :category
 	has_attached_file :image, 
-					  :styles => { medium: "500x500>", thumb: "200x200>" },
+					  :styles => { medium: "600x600>", thumb: "300x300>" },
 					  :bucket => 'rails-iphone-case'
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
